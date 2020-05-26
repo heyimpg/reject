c62a04a621e49567b3b612720cf9efcfea7a6ac6
ImPG <65141726+heyimpg@users.noreply.github.com>
2020-05-26 17:13:55 +0700

    Add files via upload

---
+++ countNumber.java
@@ -0,0 +1,16 @@
+package super_test;
+
+public class countNumber {
+    public static void main(String[] args) {
+     /*       int number =123;
+            String count= ""+number;
+        System.out.println(count.length());
+        System.out.println(number* Math.pow(10,-count.length()));*/
+       /* String a=null;
+        System.out.println(".".equals(a));*/
+        String a= "1.66";
+        System.out.println(a.substring(1,a.length()-1));
+//        System.out.println(new StringBuilder().replace(0,a.length()-2,a));
+        System.out.print("\n\n\n--------End Code--------");
+    }
+}
